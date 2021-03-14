229c3b570b077490162089d85f7e9be5f2485396
santeri <santeri.suomala@outlook.com>
2021-03-14 15:56:23 +0200

    rework file structure + add new tasks

---
+++ wks/bootstrap.sh
@@ -0,0 +1,6 @@
+echo "Installing Ansible..."
+sudo dnf install ansible -y
+echo "Installing required Ansible packages..."
+ansible-galaxy collection install community.general
+echo "Beginning playbook run..."
+ansible-playbook ansible/wks/wks.yml --ask-become-pass || echo "Cleaning up..." && rm -rf ansible && echo "All done! New shell available at next login."
